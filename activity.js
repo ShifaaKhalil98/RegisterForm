b70241437a8f10d9b0a6f72413ebbeb9195f26c2
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-02-26 17:01:20 +0200

    implemented sort algorithm

--- activity.js
+++ activity.js
@@ -1,1 +1,21 @@
-prompt("Enter any number");
+var numbers = [];
+for (i = 0; i < 3; i++) {
+  value = prompt("Enter any number");
+  numbers.push(value);
+}
+document.getElementById("original").innerText = "Original array: " + numbers;
+
+document.getElementById("sorted").innerText =
+  "Sorted array: " + sort_array(numbers);
+function sort_array(num) {
+  for (i = 0; i < num.length; i++) {
+    for (j = 0; j < num.length - i - 1; j++) {
+      if (num[j + 1] < num[j]) {
+        k = num[j];
+        num[j] = num[j + 1];
+        num[j + 1] = k;
+      }
+    }
+  }
+  return num;
+}
