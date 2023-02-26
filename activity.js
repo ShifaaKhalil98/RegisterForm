8513be9d8c8dc651ae4365ea0540bd35c26135d3
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-02-26 18:02:36 +0200

    added a function that calculates the age and returns if it is prime

--- activity.js
+++ activity.js
@@ -5,10 +5,12 @@ reverse_btn = document.getElementById("reverse");
 suffix_btn = document.getElementById("add-ay");
 var text_1 = document.getElementById("text1");
 var text_2 = document.getElementById("text2");
+var current_date = new Date();
+var current_year = current_date.getFullYear();
 //Listeners
 sort_btn.addEventListener("click", display_sorted);
 palindrome_btn.addEventListener("click", display_palindrome);
-prime_btn.addEventListener("click");
+prime_btn.addEventListener("click", display_prime);
 reverse_btn.addEventListener("click");
 suffix_btn.addEventListener("click");
 //Functions
@@ -33,17 +35,45 @@ function display_sorted() {
   text_1.innerText = "Original array: " + numbers;
   text_2.innerText = "Sorted array: " + sort_array(numbers);
 }
-function palindrome(str) {
+function is_palindrome(str) {
   reversed_str = "";
   for (i = 0; i < str.length; i++) {
     reversed_str = str[i] + reversed_str;
   }
-  return reversed_str;
+  if (str == reversed_str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 function display_palindrome() {
   value = prompt("Give a string");
-  if (value == reversed_str(value)) {
+  if (value.is_palindrome) {
     text_1.innerText = text_1 + "is a palindrome";
+    text_2.innerText = "";
+  } else {
+    text_1.innerText = text_1 + "is not a palindrome";
+    text_2.innerText = "";
+  }
+}
+function is_prime(num) {
+  for (i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function display_prime() {
+  do {
+    year_of_birth = prompt("Which year were you born?");
+    age = current_year - year_of_birth;
+  } while (current_year < year_of_birth);
+  if (age.is_prime) {
+    text_1.innerText = "Your age " + text_1 + "is a prime number";
+    text_2.innerText = "";
+  } else {
+    text_1.innerText = "Your age " + text_1 + "is not a prime number";
     text_2.innerText = "";
   }
 }
